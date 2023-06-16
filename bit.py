a74fd5cb385893ebe059c945276c936357afcb25
Mid Hunter <43461693+MidHunterX@users.noreply.github.com>
2023-06-16 14:22:47 +0530

    Update bit.py
    Added 4-Up Script and other optimizations.

--- bit.py
+++ bit.py
@@ -15,6 +15,8 @@ from os import remove
 from math import ceil
 try:
 	from PyPDF2 import PdfFileReader, PdfFileWriter
+	from PyPDF2 import PageObject
+
 except ImportError:
 	print("Umm... looks like I need PyPDF2 to access dem files bro.")
 
@@ -27,9 +29,10 @@ print("""
  |_|  |_|_|\__,_| |_|  |_|\__,_|_| |_|\__\___|_|
             Duplex N-up page (imposition) algorithm
 """)
+print("===================================================")
 
 # 1. Get PDF file and total no. of pages (n)
-pdf_document = input(("Drop file here : "))
+pdf_document = input(("Drag and Drop PDF file : "))
 if (pdf_document[0] == "\""):
 	# Remove quotes from the ends of path
 	pdf_document = pdf_document[1:-1]
@@ -73,13 +76,17 @@ for page in range(n):
 	else:
 		even_set.addPage(current_page)
 
+print("======================= LOG =======================")
+
 with open(odd_file, "wb") as out:
 	odd_set.write(out)
-	print("created", odd_file)
+	print("Separated odd pages in:", odd_file)
 
 with open(even_file, "wb") as out:
 	even_set.write(out)
-	print("created", even_file)
+	print("Separated even pages in: ", even_file)
+
+print("---------------------------------------------------")
 
 # 4. Swap every 2 neighbouring pages with each other in even set
 temp_reader = PdfFileReader(even_file)
@@ -95,7 +102,8 @@ for i in range(even_set_size):
 		temp_writer.addPage(current_page)
 
 temp_writer.write(open(even_file, "wb"))
-print("corrected", even_file)
+
+print("Swapping even pages successful in:", even_file)
 
 # 5. Append 4 pages from odd and even sets till n
 file1 = PdfFileReader(odd_file)
@@ -116,8 +124,85 @@ for i in range(n//8):
 		file2_page += 1
 
 temp_writer.write(open(temp_file, "wb"))
-print("finalized", temp_file)
+print("Created Intermediate PDF:", temp_file)
+print("---------------------------------------------------")
 
 # Cleaning up
 remove(odd_file)
 remove(even_file)
+
+# =========================================================================
+
+# 4-Up Algorithm
+# ---------------
+# (h) *---------*
+#     |    |    |
+#     |  1 | 2  |
+# (0) *---(w)---*
+#     |    |    |
+#     | 3  | 4  |
+#  `  *---------*
+# n(x,y) | 1(0,h) | 2(w,h) | 3(0,0) | 4(w,0)
+# ! This algorithm trims off at 4th multiples page no.
+
+print("Starting N-Up algorithm...")
+
+
+def four_up_pdf(input_path, output_path):
+	reader = PdfFileReader(input_path)
+	writer = PdfFileWriter()
+
+	total_pages = reader.getNumPages()
+
+	#Make a list of all pages
+	pages = []
+	for pageNum in range(reader.numPages):
+		pageObj = reader.getPage(pageNum)
+		pages.append(pageObj)
+
+	#Calculate width and height for final output page
+	width = pages[1].mediaBox.getWidth() * 2
+	height = pages[1].mediaBox.getHeight() * 2
+
+	#Create blank page to merge all pages in one page
+	merged_page = PageObject.createBlankPage(None, width, height)
+	#Loop through all pages and merge / add them to blank page
+	i=0
+	merged_page = PageObject.createBlankPage(None, width, height)
+	for page in range(len(pages)):
+		i+=1
+		if i==1:
+			x=0
+			y=float(pages[page].mediaBox.getHeight())
+			merged_page.mergeScaledTranslatedPage(pages[page], 1,x, y)
+		if i==2:
+			x=float(pages[page].mediaBox.getWidth())
+			y=float(pages[page].mediaBox.getHeight())
+			merged_page.mergeScaledTranslatedPage(pages[page], 1,x, y)
+		if i==3:
+			merged_page.mergePage(pages[page])
+			x=0
+			y=0
+			merged_page.mergeScaledTranslatedPage(pages[page], 1,x, y)
+		if i==4:
+			# merged_page.mergePage(pages[page])
+			x=float(pages[page].mediaBox.getWidth())
+			y=0
+			merged_page.mergeScaledTranslatedPage(pages[page], 1,x, y)
+			# Create new blank page on every 4th page and reset var
+			writer.addPage(merged_page)
+			merged_page = PageObject.createBlankPage(None, width, height)
+			i=0
+
+	#Create final file with one page
+	with open(output_file, 'wb') as f:
+		writer.write(f)
+
+# Usage
+input_file = temp_file
+output_file = temp_file
+print("Performing N-Up on PDF:", temp_file)
+print("Setting up pages. Please wait...")
+four_up_pdf(input_file, output_file)
+print("Final PDF created successfully:", temp_file)
+print("---------------------------------------------------")
